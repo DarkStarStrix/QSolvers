51304f735f1de0a9d4e940ec2b331cf8b0ee524b
DarkStarStrix <allanw.mk@gmail.com>
2024-03-13 18:20:08 -0400

    Revised Non-Linear_Naiver_Stokes_Equation.py

--- Quantum_Non_Linear_solvers/Non-Linear_Naiver_Stokes_Equation.py
+++ Quantum_Non_Linear_solvers/Non-Linear_Naiver_Stokes_Equation.py
@@ -1,62 +1,68 @@
 import numpy as np
 import plotly.figure_factory as ff
 from qutip import *
+import plotly.io as pio
 
 
-class Parameters:
-    def __init__(self):
-        self.Nx = self.Ny = self.Nt = 100
-        self.dt = 0.01
-        self.T = 1.0
-        self.Re = 100
+class NonLinearSchrodingerSolver:
+    def __init__(self, hamiltonian, initial_state, time_array):
+        self.hamiltonian = hamiltonian
+        self.initial_state = initial_state
+        self.time_array = time_array
+        self.solution = None
 
-
-class Simulation:
-    def __init__(self, parameters):
-        self.parameters = parameters
-
-    @staticmethod
-    def initial_condition(self, x, y):
+    def nonlinear_term(self, state, t, x, y):
+        Naiver_Stokes = 1j * (sigmax () * state - state * sigmax ())
         u = np.sin (np.pi * x) * np.cos (np.pi * y)
         v = -np.cos (np.pi * x) * np.sin (np.pi * y)
         p = -0.25 * (np.cos (2 * np.pi * x) + np.cos (2 * np.pi * y))
         return u, v, p
 
-    def non_linear_navier_stokes_equation(self, u, v, p, x, y, t):
-        dx, dy, dt = x [1] - x [0], y [1] - y [0], t [1] - t [0]
-        u_xx, u_yy = (u [2:, :] - 2 * u [1:-1, :] + u [:-2, :]) / dx ** 2, (
-                    u [:, 2:] - 2 * u [:, 1:-1] + u [:, :-2]) / dy ** 2
-        v_xx, v_yy = (v [2:, :] - 2 * v [1:-1, :] + v [:-2, :]) / dx ** 2, (
-                    v [:, 2:] - 2 * v [:, 1:-1] + v [:, :-2]) / dy ** 2
+    def solve(self, x, y):
+        self.solution = sesolve (self.hamiltonian, self.initial_state, self.time_array,
+                                 e_ops=[lambda state, t: self.nonlinear_term (state, t, x, y)])
+        if not self.solution.states:
+            raise ValueError ("No states were found. Please check the inputs.")
 
-        u_t = np.clip (
-            u_xx + u_yy - (u * (u - u [:-1, :]) / dx + v * (u - u [:, :-1]) / dy) + 1 / self.parameters.Re * (
-                        u_xx + u_yy), -np.inf, np.inf)
-        v_t = np.clip (
-            v_xx + v_yy - (u * (v - v [:-1, :]) / dx + v * (v - v [:, :-1]) / dy) + 1 / self.parameters.Re * (
-                        v_xx + v_yy), -np.inf, np.inf)
-
-        u [1:-1, 1:-1] += u_t * dt
-        v [1:-1, 1:-1] += v_t * dt
-        return u, v, p
-
-    def run(self):
-        x = np.linspace (0, 1, self.parameters.Nx)
-        y = np.linspace (0, 1, self.parameters.Ny)
-        t = np.linspace (0, self.parameters.T, self.parameters.Nt)
-
-        u, v, p = self.initial_condition (x, y)
-
-        for _ in range (int (5 * 60 / self.parameters.dt)):
-            u, v, p = self.non_linear_navier_stokes_equation (u, v, p, x, y, t)
-
-        fig = ff.create_quiver (x, y, u, v)
-        fig.show ()
-
-        return u, v, p
+    def visualize(self):
+        abs_states = [np.abs (state.full ()) for state in self.solution.states]
+        real_states = [np.real (state) for state in abs_states]
+        imag_states = [np.imag (state) for state in abs_states]
+        if real_states and imag_states:  # Check if real_states and imag_states are not empty
+            real_states_flat = np.concatenate (real_states).ravel ()
+            imag_states_flat = np.concatenate (imag_states).ravel ()
+            quiver_plot = ff.create_quiver (real_states_flat, imag_states_flat, real_states_flat, imag_states_flat,
+                                            scale=.2, line=dict (width=1), marker=dict (size=1),
+                                            name='Schrödinger Equation')
+            pio.write_html (quiver_plot, 'quiver_plot.html')
 
 
-if __name__ == "__main__":
-    parameters = Parameters ()
-    simulation = Simulation (parameters)
-    simulation.run ()
+# Define the Hamiltonian
+hamiltonian = sigmaz ()
+
+# Define the initial state
+initial_state = basis (2, 0)
+
+# Define the time array
+time_array = np.linspace (0, 10, 100)
+
+# Define x and y
+x = np.linspace (0, 1, 100)
+y = np.linspace (0, 1, 100)
+
+# Check the inputs
+assert hamiltonian is not None, "Hamiltonian is not defined"
+assert initial_state is not None, "Initial state is not defined"
+assert len (time_array) > 0, "Time array is empty"
+
+# Create an instance of the NonLinearSchrodingerSolver class
+solver = NonLinearSchrodingerSolver (hamiltonian, initial_state, time_array)
+
+# Solve the Schrödinger equation
+try:
+    solver.solve (x, y)
+except ValueError as e:
+    pass
+
+# Visualize the results
+solver.visualize ()
