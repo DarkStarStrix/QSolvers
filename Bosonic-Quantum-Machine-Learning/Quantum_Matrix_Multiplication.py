21e9677650df49ae346791b644e6085f52d0b987
DarkStarStrix <allanw.mk@gmail.com>
2024-05-03 12:45:02 -0400

    Backend Update

--- Bosonic-Quantum-Machine-Learning/Quantum_Matrix_Multiplication.py
+++ Bosonic-Quantum-Machine-Learning/Quantum_Matrix_Multiplication.py
@@ -1,9 +1,9 @@
 # create a quantum circuit to perform matrix multiplication and compare the result with classical matrix multiplication
 
-from qiskit import QuantumCircuit
-from qiskit.quantum_info import Operator
 import numpy as np
 import plotly.graph_objects as go
+from qiskit import QuantumCircuit
+from qiskit.quantum_info import Operator
 
 
 class QuantumMatrix:
@@ -22,18 +22,43 @@ def quantum_matrix_multiplication(matrix_a, matrix_b):
     qc.unitary (Operator (matrix_a.matrix), [0, 1])
     qc.unitary (Operator (matrix_b.matrix), [0, 1])
     qc.measure_all ()
-    simulator = Aer.get_backend ('qasm_simulator')
-    result = execute (qc, simulator, shots=10000).result ()
+    result = execute (qc, backend='qasm_simulator', shots=1024)
     return result.get_counts (qc)
 
 
-matrix_a = QuantumMatrix (np.array ([[1, 2], [3, 4]]))
-matrix_b = QuantumMatrix (np.array ([[5, 6], [7, 8]]))
-counts = quantum_matrix_multiplication (matrix_a, matrix_b)
-C = matrix_a * matrix_b
+def classical_matrix_multiplication(matrix_a, matrix_b):
+    return matrix_a * matrix_b
 
-fig = go.Figure (data=[
-    go.Bar (name='Quantum', x=list (counts.keys ()), y=list (counts.values ())),
-    go.Bar (name='Classical', x=list (C.matrix.flatten ()), y=list (C.matrix.flatten ()))
-])
-fig.show ()
+
+def is_unitary(matrix):
+    matrix = np.array (matrix)
+    return np.allclose (np.eye (matrix.shape [0]), matrix @ matrix.T.conj ())
+
+
+# Define unitary matrices
+matrix_a = QuantumMatrix (np.array ([[1 / np.sqrt (2), 1 / np.sqrt (2)], [1 / np.sqrt (2), -1 / np.sqrt (2)]]))
+matrix_b = QuantumMatrix (np.array ([[1 / np.sqrt (2), 1 / np.sqrt (2)], [1 / np.sqrt (2), -1 / np.sqrt (2)]]))
+
+quantum_result = None
+classical_result = None
+
+# Check if the matrix is unitary
+if not is_unitary (matrix_a.matrix):
+    print ("Matrix A is not unitary. Please provide a unitary matrix.")
+else:
+    # Perform quantum matrix multiplication
+    quantum_result = quantum_matrix_multiplication (matrix_a, matrix_b)
+
+    # Perform classical matrix multiplication
+    classical_result = classical_matrix_multiplication (matrix_a, matrix_b)
+
+# Compare results
+if quantum_result is not None and classical_result is not None:
+    fig = go.Figure (data=[
+        go.Bar (name='Quantum', x=list (quantum_result.keys ()), y=list (quantum_result.values ())),
+        go.Bar (name='Classical', x=list (classical_result.matrix.flatten ()),
+                y=list (classical_result.matrix.flatten ()))
+    ])
+    fig.show ()
+else:
+    print ("Quantum or classical result is None. Please provide unitary matrices.")
